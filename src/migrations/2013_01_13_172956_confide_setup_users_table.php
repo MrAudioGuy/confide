6c0b20b23e60affc3a151e76db63a8c71c1473f4
Mustafa Talaeezadeh Khouzani <brother.t@live.com>
2014-12-31 15:23:03 +0330

    improvising migrations

--- src/migrations/2013_01_13_172956_confide_setup_users_table.php
+++ src/migrations/2013_01_13_172956_confide_setup_users_table.php
@@ -1,44 +1,98 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
+	use Illuminate\Database\Migrations\Migration;
 
-class ConfideSetupUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        // Creates the users table
-        Schema::create('users', function ($table) {
-            $table->increments('id');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('confirmation_code');
-            $table->string('remember_token')->nullable();
-            $table->boolean('confirmed')->default(false);
-            $table->timestamps();
-        });
+	class ConfideSetupUsersTable extends Migration
+	{
+		/**
+		 * Run the migrations.
+		 */
+		public function up()
+		{
+			// Creates the users table
+			Schema::create('users', function ($table)
+			{
+				$table->bigIncrements('id')->unsigned();
+				$table->string('username')->unique();
+				$table->string('email')->unique();
+				$table->string('mobile')->unique();
+				$table->string('password');
+				$table->string('secret');
+				$table->string('remember_token')->nullable();
+				$table->integer('status')->default(1);
+				$table->timestamps();
+				$table->softDeletes();
+			});
 
-        // Creates password reminders table
-        Schema::create('password_reminders', function ($table) {
-            $table->string('email');
-            $table->string('token');
-            $table->timestamp('created_at');
-        });
-    }
+			// Creates password reminders table
+			Schema::create('reminders', function ($table)
+			{
+				$table->bigIncrements('id')->unsigned();
+				$table->string('email')->nullable();
+				$table->string('mobile')->nullable();
+				$table->bigInteger('user_id')->unsigned()->nullable();
+				$table->string('type')->nullable();
+				$table->string('token')->unique();
+				$table->timestamps();
+				$table->softDeletes();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('password_reminders');
-        Schema::drop('users');
-    }
-}
+				$table->foreign('user_id')
+					  ->references('id')->on('users')
+					  ->onDelete('cascade')
+					  ->onUpdate('cascade');
+			});
+
+			// Create list of approved oath providers
+			Schema::create('oathproviders', function ($table)
+			{
+				$table->bigIncrements('id')->unsigned();
+				$table->string('provider');
+				$table->string('allowed_services');
+				$table->string('url');
+				$table->timestamps();
+				$table->softDeletes();
+			});
+
+			// Create OAuth specific features
+			Schema::create('oath', function ($table)
+			{
+				$table->bigIncrements('id')->unsigned();
+				$table->bigInteger('user_id')->unsigned();
+				$table->bigInteger('provider_id')->unsigned();
+				$table->string('services');
+				$table->string('token');
+				$table->timestamps();
+				$table->softDeletes();
+
+				$table->foreign('user_id')
+					  ->references('id')->on('users')
+					  ->onDelete('cascade')
+					  ->onUpdate('cascade');
+
+				$table->foreign('provider_id')
+					  ->references('id')->on('oathproviders')
+					  ->onDelete('cascade')
+					  ->onUpdate('cascade');
+			});
+		}
+
+		/**
+		 * Reverse the migrations.
+		 */
+		public function down()
+		{
+			Schema::table('reminders', function($table){
+				$table->dropForeign('reminders_user_id_foreign');
+			});
+			Schema::table('oath', function($table)
+			{
+				$table->dropForeign('oath_user_id_foreign');
+				$table->dropForeign('oath_provider_foreign');
+			});
+
+			Schema::drop('oathproviders');
+			Schema::drop('oath');
+			Schema::drop('reminders');
+			Schema::drop('users');
+		}
+	}
